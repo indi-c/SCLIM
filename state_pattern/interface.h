ea3bb000a80e2e35f40ca04977551a24ca27727a
indi <indigoconnelly@pm.me>
2024-02-15 01:47:23 +0000

    basic main and header for interface

---
+++ state_pattern/interface.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct interface;
+
+void initInterface(struct interface* interface);
+static void destroyInterface(struct interface* interface);
---
+++ state_pattern/main.c
@@ -0,0 +1,11 @@
+// #include "interface.h"
+
+int main()
+{
+    // instantiate interface an interface with function
+    // this will be the main entry point for the program
+    // and will be the only function called from main
+    // handling input and output
+    // calling the appropriate functions
+    return 0;
+}
